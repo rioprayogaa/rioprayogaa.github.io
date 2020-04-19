0ff32d927561af426f01f3cd14ff676ada2164a7
Rio Prayoga <r.prayoga2328@gmail.com>
2020-04-19 18:26:48 +0700

    responsive navbar revisi

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,15 +1,17 @@
 function myFunction(x) {
     if (x.matches) { // If media query matches
+     $(".categoryRes").removeAttr('data-toggle','dropdown');
       var iconCL = $('#iconCL').removeClass("fa-th-large");
       iconCL.addClass("fa-sign-in-alt");
       document.getElementById('txtCategory-login').innerHTML = "Login";
     } else {
+      $(".categoryRes").attr('data-toggle','dropdown');
         var iconCL = $('#iconCL').removeClass("fa-sign-in-alt");
         iconCL.addClass("fa-th-large");
         document.getElementById('txtCategory-login').innerHTML = "Category";
     }
   }
   
-  var x = window.matchMedia("(max-width: 600px)")
+  var x = window.matchMedia("(max-width: 768px)")
   myFunction(x) // Call listener function at run time
   x.addListener(myFunction)
