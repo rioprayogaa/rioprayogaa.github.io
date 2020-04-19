9eff59ea2021319d57dd5e35092d63298aad0e57
Rio Prayoga <r.prayoga2328@gmail.com>
2020-04-19 12:52:28 +0700

    mencoba mengubah Category jadi Login

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,1 +1,12 @@
-
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+      document.getElementById('category-login').innerHTML = "Login";
+    } else {
+    
+        document.getElementById('category-login').innerHTML = "Categori";
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 600px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction)
